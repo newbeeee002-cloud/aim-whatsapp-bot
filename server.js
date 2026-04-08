fca6e82ceb11babc800bfd28eb84627df17a63bc
newbeeee002-cloud <aimlogistics002@gmail.com>
2026-04-08 17:58:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 10000;
+
 app.get("/", (req, res) => {
   res.send("AIM WhatsApp Bot Running 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server started");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started on port ${PORT}`);
 });
